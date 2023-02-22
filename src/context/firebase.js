a1506839e8c5ca0b74d6a1ec609df0530d16bf1e
unknown <gbemilesanmi@gmail.com>
2023-02-22 12:44:27 +0100

    formatted gitignore

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -2,15 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import {getFirestore} from 'firebase/firestore'
 import { getStorage} from 'firebase/storage'
+import {firebaseConfig} from '../config/index.js'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDHKtwqIRm_YyJVl0lwx_rUJQutYpn54X4",
-  authDomain: "foursquare-fdda4.firebaseapp.com",
-  projectId: "foursquare-fdda4",
-  storageBucket: "foursquare-fdda4.appspot.com",
-  messagingSenderId: "24427042224",
-  appId: "1:24427042224:web:2c8e0345241eb7fe987275"
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
